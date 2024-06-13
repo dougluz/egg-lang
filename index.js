28378b18e24dceb3b2c35f0e02c7deb486d929e1
Douglas Luz <douglas.luz@rdstation.com>
2024-06-13 19:15:18 -0300

    added array support

--- index.js
+++ index.js
@@ -138,6 +138,19 @@ topScope.print = value => {
   return value;
 }
 
+topScope.array = (...args) => args
+
+topScope.length = (args) => args.length;
+
+topScope.element = (args, position) => {
+  if (typeof args != "object" || typeof position  != "number") {
+    console.log(typeof(args), typeof(position));
+    throw new TypeError("Wrong type of argument in element");
+  }
+
+  return args[position]
+}
+
 function evaluate(expr, scope) {
   if (expr.type == "value") {
     return expr.value;
@@ -175,15 +188,30 @@ function run(program) {
 //      print(total))
 //   `);
 
-run(`
-  do(define(plusOne, fun(a, +(a, 1))),
-     print(plusOne(10)))
-  `);
+// run(`
+//   do(define(plusOne, fun(a, +(a, 1))),
+//      print(plusOne(10)))
+//   `);
+
+// run(`
+//   do(define(pow, fun(base, exp,
+//         if(==(exp, 0),
+//           1,
+//           *(base, pow(base, -(exp, 1)))))),
+//       print(pow(2, 10)))
+//   `);
+
+// run(`
+//     print(array(1,2,3))
+//   `)
 
 run(`
-  do(define(pow, fun(base, exp,
-        if(==(exp, 0),
-          1,
-          *(base, pow(base, -(exp, 1)))))),
-      print(pow(2, 10)))
+  do(define(sum, fun(array,
+       do(define(i, 0),
+          define(sum, 0),
+          while(<(i, length(array)),
+            do(define(sum, +(sum, element(array, i))),
+               define(i, +(i, 1)))),
+          sum))),
+     print(sum(array(1, 2, 3))))
   `);
